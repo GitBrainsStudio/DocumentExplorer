e3260737b235b4800f3ee23ca2026cdff90473db
Developer <gitbrains@gmail.com>
2020-07-09 16:02:32 +0500

    added component: 'file-preview', 'folder-list'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FolderListComponent } from './folder-list/folder-list.component';
+import { FilePreviewComponent } from './file-preview/file-preview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FolderListComponent,
+    FilePreviewComponent
   ],
   imports: [
     BrowserModule,
