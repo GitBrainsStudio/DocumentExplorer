f9f59dfe44559ed00982c706b50b38e540691921
Developer <gitbrains@gmail.com>
2020-07-09 16:06:19 +0500

    added route to 'folder-reader' and redirect '**' to 'folder-reader'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FolderReaderPageComponent } from './folder-reader-page/folder-reader-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'folder-reader', component: FolderReaderPageComponent,
+  },
+
+  {
+    path: '**', redirectTo: 'folder-reader'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
